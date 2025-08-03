856f0949a9aafc874e5d061fcb7f5d1c4f2e8284
Jonissoncruz <jonisson@yahoo.com.br>
2025-08-03 03:34:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-  const links = document.querySelectorAll('.nav-links a');
-  for (const link of links) {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      const section = document.querySelector(this.getAttribute('href'));
-      section.scrollIntoView({ behavior: 'smooth' });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
-  }
+  });
 });
